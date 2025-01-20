3fc934c8176a1bf9b4a9393affb92c9332b3efe9
Abenezer8636 <158500295+Abenezer8636@users.noreply.github.com>
2025-01-20 11:40:22 +0300

    BinarySearch.py
    Binary Search in Python

--- demo.py
+++ demo.py
@@ -1,1 +1,23 @@
-print('Hello World')
+
+def search(list,n):
+    low = 0
+    upp = len(list) -1
+    while low <= upp:
+        mid = (low + upp) // 2
+        if list[mid] == n:
+            return mid
+        elif list[mid] < n:
+            low = mid + 1
+        else:
+            upp = mid-1
+    return -1 # if not found in a list
+
+list = [56,67,89,45,34,22,85,23,5]
+list.sort()
+n = 89
+poss = search(list,n)
+
+if poss != -1:
+    print(f'{n} is found at position {poss}')
+else:
+    print('Not Found')
